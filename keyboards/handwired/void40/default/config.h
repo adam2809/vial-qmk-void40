80c6c4d6d02226da66f0c290420b6d2be7c44684
Adam Kulesza <adam.kule@gmail.com>
2025-05-19 19:03:27 +0200

    More correct pins

--- keyboards/handwired/void40/default/config.h
+++ keyboards/handwired/void40/default/config.h
@@ -23,8 +23,8 @@
 #define DEBOUNCE 5
 
 /* Keyboard Matrix Assignments */
- #define MATRIX_ROW_PINS { D3, D2, D1, D0 }
- #define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5, F4, F5, F6, F7, B1, B3 }
+ #define MATRIX_ROW_PINS { D3, D2, D1, C6 }
+ #define MATRIX_COL_PINS { D4, D0, D7, E6, B4, B5, F4, F5, F6, F7, B1, B3 }
 
 /* Rotary Encoder Support */
 #define ENCODERS_PAD_A { B6 }
