3477bad6b5d74377ff71648b3e96b67a18bd1867
Adam Kulesza <adam.kule@gmail.com>
2025-05-20 02:53:10 +0200

    sets fps as secod all transparent layer

--- keyboards/handwired/void40/keymaps/vial/keymap.c
+++ keyboards/handwired/void40/keymaps/vial/keymap.c
@@ -2,8 +2,8 @@
 
 // Defines names for use in layer keycodes and the keymap
 enum layer_names {
-    _FPS,
     _BASE,
+    _FPS,
     _RAISE,
     _LOWER,
     _ADJUST
@@ -21,19 +21,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Ctl│   │GUI│Alt│Lwr│Spc│Ent│Rse│ ← │ ↓ │ ↑ │ → │
      * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
      */
-    [_FPS] = LAYOUT_ortho_4x12(
-        KC_NO,    KC_NO,    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_NO, KC_DEL,
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
-        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO, KC_NO
-    ),
     [_BASE] = LAYOUT_ortho_4x12(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,     KC_Y,     KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,     KC_H,     KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,     KC_N,     KC_M,       KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
         KC_LCTL, KC_NO,   KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,   KC_ENT,   MO(_RAISE), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
-
+    [_FPS] = LAYOUT_ortho_4x12(
+        KC_NO,    KC_NO,    RGB_TOG,  RGB_MOD,  RGB_HUI,  RGB_HUD,  RGB_SAI,  RGB_SAD,  RGB_VAI,  RGB_VAD,  KC_NO, KC_DEL,
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, KC_NO,
+        KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_TRNS,  KC_NO,    KC_NO,    KC_NO, KC_NO
+    ),
     [_LOWER] = LAYOUT_ortho_4x12(
         KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_TRNS,
         KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
@@ -58,10 +57,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
-}
-
-void keyboard_post_init_user(void) {
-    set_single_persistent_default_layer(_BASE);
 }
 
 /*
